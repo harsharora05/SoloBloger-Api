a9b8216a34c912539e8ef7fff16aad72a00ca8d3
harsharora05 <hararora12@gmail.com>
2024-12-28 12:22:21 +0530

    login view change

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 from user.api.views import Register,logout,login
 
 urlpatterns = [
